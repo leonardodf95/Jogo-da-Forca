ddb72f46857357d37fb69b1564a349958dd96d08
Leonardo Dias Ferreira <leonardodf95@gmail.com>
2022-02-23 19:08:09 -0300

    jogo finalizado com tratamento de erros, sem parte visual

--- script.js
+++ script.js
@@ -13,7 +13,15 @@ let vidas = 7;
 let letra;
 let tema;
 let letras_erradas = [];
-let novamente;
+let n;
+let palavra;
+let palavra_resultado;
+let contador;
+let verificaLetra = true;
+let verificaNumeros = true;
+let arraydenumeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let letraNovamente = false;
+let letrasErrada = false;
 
 function randomm () {
     return Math.floor(Math.random() * 10);
@@ -46,51 +54,105 @@ function checador_de_letra (letra, arraypalavra, palavra_resultado) {
         if (letra == arraypalavra[i]){
             if (letra !== palavra_resultado[i]){
                 acertos++;
+            } else if (letra == palavra_resultado[i]) {
+                letraNovamente = true;
             }
             palavra_resultado[i] = letra;
             acertou = true;
         }    
         
     }
-    console.log(acertou);
-    if (acertou == false) {
-        letras_erradas.push(letra);
-        alert("Você errou!");
-        vidas--;
-        if (vidas == 0) {
-            alert("Você perdeu!");
-        } else {
-            alert("Vidas restantes: " + vidas);
-        }
-        alert("letras erradas: " + letras_erradas);
+    if (letraNovamente == true) {
+        alert("Você já digitou essa letra!");
+        letraNovamente = false;
     }
-    if (acertou == true) {
-        alert(palavra_resultado);
+    console.log(acertou);
+    somenteLetras(letra);
+    if (verificaLetra == true){
+        if (acertou == false) {
+            for(i=0;i <= letras_erradas.length; i++){
+                if (letra !== letras_erradas[i]){
+                    letrasErrada = true;
+                } else if (letra == letras_erradas[i]) {
+                    letrasErrada = false;
+                    letraNovamente = true;                     
+                }
+            }
+            if (letrasErrada == true) {
+                if(letraNovamente == false){
+                    letras_erradas.push(letra);
+                    alert("Você errou!");
+                    vidas--;
+                    letrasErrada = false;
+                }
+            } else if (letraNovamente == true) {
+                alert("Você já digitou essa letra!");
+                letraNovamente = false;
+            }    
+            if (vidas == 0) {
+                alert("Você perdeu!");
+            } else {
+                alert("Vidas restantes: " + vidas);
+            }
+            alert("letras erradas: " + letras_erradas);
+        }
+        if (acertou == true) {
+            alert(palavra_resultado);
+        }
+    } 
+}
+function zera_jogo() {
+    if (vidas == 0 || acertos != 0) {
+        vidas = 7;
+        acertos = 0;
+        letras_erradas = [];
+    }
+}
+function somenteLetras (letra) {
+    verificaLetra = true;
+    verificaNumeros = true;
+    for (i=0; i<= arraydenumeros.length; i++){
+        if (letra === arraydenumeros[i]){
+            verificaNumeros = false
+        }
+    }
+    if (letra.length > 1){
+        verificaLetra = false;
+        return alert("Erro! Você digitou mais de uma letra!");        
+    } else if (letra.length == 0) {
+        verificaLetra = false;
+        return alert("Erro! Você não digitou nada!");
+    } else if (verificaNumeros == false) {
+        verificaLetra = false;
+        return alert("Erro! Você digitou um número!");
     }
 }
 
-
-do {
-    vidas = 7;
-    let n = parseInt(prompt("Escolha um tema pelo seu número correspondente: "));
+function inicia_jogo() {
+    zera_jogo();
+   
+    n = parseInt(prompt("Escolha um tema pelo seu número correspondente: "));
     tema = temaStr(n);
     console.log(tema);
-    let palavra = sorteador_de_palavra(n);
+    palavra = sorteador_de_palavra(n);
     console.log(palavra);
     arraypalavra = palavra.split("");
-    let palavra_resultado = arraypalavra.map (char => {return '_'});
+    palavra_resultado = arraypalavra.map (char => {return '_'});
     console.log(palavra_resultado);
     alert("O tema escolhido foi: " + tema);
     alert("Sua palavra possui " + arraypalavra.length + " letras.");
-    let contador = arraypalavra.length;
+    contador = arraypalavra.length;
         while (vidas > 0) {
             if (acertos < contador) {
-            letra = prompt("Insira a letra:");
-            checador_de_letra(letra, arraypalavra, palavra_resultado);
+                letra = prompt("Insira a letra:");
+                checador_de_letra(letra, arraypalavra, palavra_resultado);
+                
             } else {
                 vidas = 0;
             }
         };
-       
-    novamente = prompt("Deseja jogar novamente? (s/n)").toUpperCase;
-} while (novamente === "S");
+    if(acertos == contador) {
+        alert("Parabéns você ganhou!");
+    }
+}       
+
